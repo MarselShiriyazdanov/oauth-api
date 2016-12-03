d867e97502c8fa54844aef9345a9e00af0362392
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2016-12-03 19:28:31 +0300

    set token unless present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::ImplicitRender
 
+  before_action :set_token, if: :current_user
+
   acts_as_token_authentication_handler_for User, fallback: :none
 
   respond_to :json
@@ -11,5 +13,11 @@ class ApplicationController < ActionController::API
     else
       super
     end
+  end
+
+  # sets authentication_token so we can share DB between react and non-react version
+  # demo purposes only
+  def set_token
+    current_user.update(:authentication_token, Devise.friendly_token) unless current_user.authentication_token
   end
 end
